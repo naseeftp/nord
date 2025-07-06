ab21ebe150dddf07dccd8cdbfc628cf4fcdcdc71
naseeftp <naseeftp35@gmail.com>
2025-07-06 18:35:47 +0530

    after uri added tro env

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const env=require("dotenv").config();
 passport.use(new GoogleStrategy({
    clientID:process.env.GOOGLE_CLIENT_ID,
    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: 'https://naseef.online/auth/google/callback',
+   callbackURL: process.env.GOOGLE_CALLBACK_URI,
    scope:['profile','email'],
    prompt: 'select-account'
 },
